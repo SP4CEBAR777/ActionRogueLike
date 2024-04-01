71c7c0f7c3b30c9581cc8c8ec856ee88ced39b33
Felix <foelixwg00@gmail.com>
2024-04-01 17:05:56 +0800

    Movement/PrimaryAttack

---
+++ ActionRogueLike/Source/ActionRogueLike/Private/SCharacter.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ASCharacter::ASCharacter() {
+  // Set this character to call Tick() every frame.  You can turn this off to
+  // improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
+
+  SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+  SpringArmComp->SetupAttachment(RootComponent);
+
+  CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+  CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay() { Super::BeginPlay(); }
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(
+    UInputComponent *PlayerInputComponent) {
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+  PlayerInputComponent->BindAxis("MoveForward", this,
+                                 &ASCharacter::MoveForward);
+  PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+
+  PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+  PlayerInputComponent->BindAxis("LookUp", this,
+                                 &APawn::AddControllerPitchInput);
+
+  PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this,
+                                   &ASCharacter::PrimaryAttack);
+  PlayerInputComponent->BindAction("Jump", IE_Pressed, this,
+                                   &ASCharacter::Jump);
+}
+
+void ASCharacter::MoveForward(float value) {
+  FRotator ControlRot = GetControlRotation();
+  ControlRot.Pitch = 0.0f;
+  ControlRot.Roll = 0.0f;
+  AddMovementInput(ControlRot.Vector(), value);
+}
+
+void ASCharacter::MoveRight(float value) {
+  FRotator ControlRot = GetControlRotation();
+  ControlRot.Pitch = 0.0f;
+  ControlRot.Roll = 0.0f;
+
+  FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+  AddMovementInput(RightVector, value);
+}
+
+void ASCharacter::PrimaryAttack() {
+  FVector HandLoc = GetMesh()->GetSocketLocation("Muzzle_01");
+  FTransform SpawnTM = FTransform(GetActorRotation(), HandLoc);
+  FActorSpawnParameters SpawnParams;
+  SpawnParams.SpawnCollisionHandlingOverride =
+      ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+  GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+}
